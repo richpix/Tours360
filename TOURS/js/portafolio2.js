ef1c2b3edde576386df5f2d5bbf0c740a36fcf85
richpix <pool.ricardo.1fm@gmail.com>
2025-10-30 15:41:59 -0500

    Add files via upload

---
+++ TOURS/js/portafolio2.js
@@ -0,0 +1,120 @@
+const escenasPortafolio = {
+  
+  "atrio": {
+    "title": "Iglesia",
+    "type": "equirectangular",
+    "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c1atrio.jpg",
+    
+    "hotSpots": [
+
+        crearHotspot(-60, -10, "atrioizq","ir" ),
+        crearHotspot(3, -30, "enmedio", ),
+        crearHotspot(56, -18, "atrioder", )
+     
+    ]
+        },
+
+        
+  "atrioizq": {
+          "title": "Iglesia",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c1atriozq.jpg",
+
+          // --Configuracion de donde mira la camara al cambiar de escena-
+          "yaw": 120,  
+          "pitch": -5,
+          "hfov": 90, //zoom--- max 120
+          //-------------------------------
+          "hotSpots": [
+            crearHotspot(128, -2, "atrio", ),
+            crearHotspot(96, -22, "enmedio", )
+          ]
+        },
+            
+         
+
+        "enmedio": {
+          "title": "centro",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c1atriomedio.jpg",
+          "hotSpots": [
+
+            crearHotspot(-10, 180, "atrio", "Volver a atrio"),
+            crearHotspot(-20, 100, "atrioder", "ir a la derecha"),
+            crearHotspot(-25, -90, "atrioizq", "ir a la izquierda"),
+            crearHotspot(-15, -50, "pasillomed", "Ir al pasillo medio")
+          ]
+        },
+            
+
+        "atrioder": {
+          "title": "derecha",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c1atrioder.jpg",
+          "hotSpots": [
+            crearHotspot( -170, -5, "atrio", "Volver al atrio"),
+            crearHotspot(-130, -30, "enmedio", "Ir al centro"),
+            crearHotspot(-40, -10, "pasilloder", "Ir al pasillo derecha")
+          ]
+        },
+
+        
+        "pasilloder": {
+          "title": "pasillo derecha",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c2pasilloder.jpg",
+          "hotSpots": [
+            crearHotspot(170, -20, "atrioder", "Volver al atrio derecha"),
+            crearHotspot(10, -30, "pasilloder2", "Ir al siguiente pasillo")
+          ]
+        },  
+
+        "pasilloder2": {
+          "title": "pasillo derecha",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c2pasilloder2.jpg",
+          "hotSpots": [
+            crearHotspot( -170, -25, "pasilloder", "Volver al pasillo derecha"),
+            crearHotspot(-47, -22, "pasilloflor", "Ir al florero")
+          ]
+        },
+
+        "pasillomed": {
+          "title": "pasillo medio",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c2pasillomedio.jpg",
+          "hotSpots":  [
+            crearHotspot(178, -23, "enmedio", "Volver al centro"),
+            crearHotspot(0, -28, "pasillomed2", "Ir al siguiente pasillo")
+          ]
+        },
+           
+
+        "pasillomed2": {
+          "title": "pasillo medio",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c2pasillomedio2.jpg",
+          "hotSpots": [
+            crearHotspot(179, -25, "pasillomed", "Volver al pasillo medio"),
+            crearHotspot(27, -26, "pasilloflor", "Ir al florero")
+          ]
+        },
+
+        "pasilloflor": {
+          "title": "florero",
+          "type": "equirectangular",
+          "panorama": "https://360tours.com.mx/wp-content/uploads/2025/10/c2pasillomedio3.jpg",
+          "hotSpots": [
+            crearHotspot(130, -20, "pasilloder2", "Volver al pasillo derecha"),
+            crearHotspot(-146, -22, "pasillomed2", "Volver al pasillo medio")    
+          
+          ]
+        }
+  
+  // ... todas las demás escenas
+};
+
+
+
+// 🔹 Inicializar el visor
+crearVisor('tour', escenasPortafolio, 'atrio');
